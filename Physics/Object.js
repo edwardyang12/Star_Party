655d81d4c74f40bccf1854519ab5305f0ff71481
unknown <edwardyang016@gmail.com>
2020-01-29 16:18:57 -0800

    Updated values

--- Physics/Object.js
+++ Physics/Object.js
@@ -1,35 +1,61 @@
-// data about a point particle
-var position;
-var velocity;
-var mass;
+// Object that has position, velocity, acceleration, mass in up to 3 dimensions
+// with their associated accessors and getter methods
 
-function Object(positionX, positionY, mass){
-    this.position = [positionX,positionY];
-    this.velocity = [0,0];
-    this.mass = mass;
+class Object{
+
+    constructor(position, mass, velocity = null, acceleration = null){
+        this.position = position;
+        this.velocity = velocity;
+        this.mass = mass;
+        this.acceleration = acceleration
+    }
+
+    get getPosition(){
+        return this.position;
+    }
+
+    get getVelocity(){
+        if (this.velocity==null) {
+            return [0,0,0];
+        }
+        return this.velocity;
+    }
+
+    get getAcceleration(){
+        if (this.acceleration==null) {
+            return [0,0,0];
+        }
+        return this.acceleration;
+    }
+
+    get getMass(){
+        return this.mass;
+    }
+
+    set changeVelocity(value){
+        this.velocity = value;
+        return 1; //1 for success
+    }
+
+    set changePosition(value){
+        this.position = value;
+        return 1; //1 for success
+    }
+
+    set changeAcceleration(value){
+        this.acceleration = value;
+        return 1; //1 for success
+    }
+
+    set changeMass(value){
+        this.mass = value;
+        return 1; //1 for success
+    }
 }
 
-function Object(positionX, positionY, velocityX, velocityY, mass){
-    this.position = [positionX,positionY];
-    this.velocity = [velocityX,velocityY];
-    this.mass = mass;
-}
-
-function getPosition(){
-  return this.position;
-}
-
-function getVelocity(){
-  return this.velocity;
-}
-
-function getMass(){
-  return this.mass;
-}
-
-
-// TODO: add adjustments to velocity and position
-
-function addVelocity(){
-
-}
+module.exports.Object = Object;
+// var position = [1,2,3 ];
+// const JOJO = new Object(position,5);
+// console.log(JOJO.getVelocity);
+// JOJO.changeVelocity= [9,9,9];
+// JOJO.getVelocity;
--- Physics/kinematics.js
+++ Physics/kinematics.js
@@ -1,14 +1,35 @@
+var a = require('./Object.js'); //import object class
+
+var position = [1,2,3 ];
+const JOJO = new a.Object(position,5);
+console.log(JOJO.getPosition);
+
+JOJO.changeVelocity = [9,9,9];
+positionVCalc(JOJO,1);
+console.log(JOJO.getPosition);
+
 //i assume time is tick based so we can figure that out later
 // TODO: connecting time to the tick based system
-function positionCalc(position, velocity, time){
-  position = velocity * time * position;
-  return position;
-}
-function velocityCalc(velocity, acceleration, time){
-  velocity = acceleration, time, velocity;
-  return velocity
+
+// positionCalc and velocityCalc update values based on time 
+function positionCalc(object, time){
+    var index;
+    var temp = object.getPosition;
+    for(index = 0; index<object.getVelocity.length;index++){
+        temp[index] = temp[index] + object.getVelocity[index]*time
+          + object.getAcceleration[index]*time*time/2;
+    }
+    object.changePosition = temp;
 }
 
+function velocityCalc(object, time){
+    var index;
+    var temp = object.getVelocity;
+    for(index = 0; index<object.getAcceleration.length;index++){
+        temp[index] = temp[index] + object.getAcceleration[index]*time;
+    }
+    object.changeVelocity = temp;
+}
 
 
 function distanceCalc(position, velocity, mass, endPosition){
